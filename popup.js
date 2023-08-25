d90bf80dc8227964af648fad88315b29dd30bf53
ClassiCcOwl <khavari.7878@yahoo.com>
2023-08-26 01:47:43 +0330

    sort results based on coin name

--- popup.js
+++ popup.js
@@ -30,11 +30,11 @@ document.addEventListener("DOMContentLoaded", async () => {
       const buy_btn = document.createElement("button");
       const td_sell_buy = document.createElement("td");
 
-      buy_btn.style.background = "red"
-      buy_btn.innerText= "BUY"
+      buy_btn.style.background = "red";
+      buy_btn.innerText = "BUY";
 
-      sell_btn.style.background = "green"
-      sell_btn.innerText = "SELL"
+      sell_btn.style.background = "green";
+      sell_btn.innerText = "SELL";
 
       div_broker_name.innerText = broker;
       div_coin_name.innerText = coin;
@@ -58,8 +58,8 @@ document.addEventListener("DOMContentLoaded", async () => {
       td_buy.append(buy);
       td_sell.append(sell);
 
-      td_sell_buy.appendChild(buy_btn)
-      td_sell_buy.appendChild(sell_btn)
+      td_sell_buy.appendChild(buy_btn);
+      td_sell_buy.appendChild(sell_btn);
 
       tr.appendChild(td_broker_image);
       tr.appendChild(td_broker_name);
@@ -67,8 +67,107 @@ document.addEventListener("DOMContentLoaded", async () => {
       tr.appendChild(td_coin_name);
       tr.appendChild(td_sell);
       tr.appendChild(td_buy);
-      tr.appendChild(td_sell_buy)
+      tr.appendChild(td_sell_buy);
       mainTable.appendChild(tr);
     }
   }
+});
+
+
+
+
+
+
+const jsonResponse = z;
+function sortByPriceAsc(a, b) {
+  return a.sellPrice - b.sellPrice;
+}
+
+function sortByPriceDec(a, b) {
+  return b.buyPrice - a.buyPrice;
+}
+
+// Function to get all prices of a specific coin across brokers
+function getPricesOfCoin(coin) {
+  const prices = [];
+
+  for (const exchange in jsonResponse.exchangesPrices) {
+    if (jsonResponse.exchangesPrices.hasOwnProperty(exchange)) {
+      const exchangeData = jsonResponse.exchangesPrices[exchange];
+      const coinPrices = exchangeData.prices[coin];
+
+      if (coinPrices) {
+        for (const currency in coinPrices) {
+          if (coinPrices.hasOwnProperty(currency)) {
+            const priceData = coinPrices[currency];
+            const buyPrice = priceData.buy; // Change to "sell" if needed
+            const sellPrice = priceData.sell;
+
+            prices.push({
+              broker: exchange,
+              coin: coin,
+              currency: currency,
+              buyPrice: buyPrice,
+              sellPrice: sellPrice,
+            });
+          }
+        }
+      }
+    }
+  }
+
+  return prices;
+}
+
+// Example usage: Get prices of "btc" coin across brokers and sort them
+const coinPrices = getPricesOfCoin("btc");
+// Separate prices by type: Sell IRT, Sell USDT, Buy IRT, Buy USDT
+let sellIRTPrices = coinPrices.filter(
+  (price) => price.currency === "irt" && price.sellPrice
+);
+let sellUSDTPrices = coinPrices.filter(
+  (price) => price.currency === "usdt" && price.sellPrice
+);
+let buyIRTPrices = coinPrices.filter(
+  (price) => price.currency === "irt" && price.buyPrice
+);
+let buyUSDTPrices = coinPrices.filter(
+  (price) => price.currency === "usdt" && price.buyPrice
+);
+
+sellIRTPrices.sort(sortByPriceAsc);
+sellUSDTPrices.sort(sortByPriceAsc);
+buyIRTPrices.sort(sortByPriceDec);
+buyUSDTPrices.sort(sortByPriceDec);
+
+// Print sorted prices
+console.log("Sell IRT Prices:");
+sellIRTPrices.forEach((price) => {
+  console.log(
+    `Broker: ${price.broker}, Coin: ${price.coin}, Sell Price (IRT): ${price.sellPrice}`
+  );
+});
+
+// Print sorted prices for Sell USDT
+console.log("Sell USDT Prices:");
+sellUSDTPrices.forEach((price) => {
+  console.log(
+    `Broker: ${price.broker}, Coin: ${price.coin}, Sell Price (USDT): ${price.sellPrice}`
+  );
+});
+
+// Print sorted prices for Buy IRT
+console.log("Buy IRT Prices:");
+buyIRTPrices.forEach((price) => {
+  console.log(
+    `Broker: ${price.broker}, Coin: ${price.coin}, Buy Price (IRT): ${price.buyPrice}`
+  );
+});
+
+// Print sorted prices for Buy USDT
+console.log("Buy USDT Prices:");
+buyUSDTPrices.forEach((price) => {
+  console.log(
+    `Broker: ${price.broker}, Coin: ${price.coin}, Buy Price (USDT): ${price.buyPrice}`
+  );
 });
