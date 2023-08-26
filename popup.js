6bea7127555edd83817fcc5420c4569bd492b80b
= <=>
2023-08-26 10:19:10 +0330

    alot of changes

--- popup.js
+++ popup.js
@@ -1,9 +1,4 @@
 const inputCoin = document.querySelector("input#coin");
-
-const tableIrtSell = document.querySelector("table#irtSell");
-const tableIrtBuy = document.querySelector("table#irtSell");
-const tableUsdtSell = document.querySelector("table#irtSell");
-const tableUsdtBuy = document.querySelector("table#irtSell");
 let res = "";
 
 async function fetchData() {
@@ -32,8 +27,6 @@ function sortByPriceAsc(a, b) {
 function sortByPriceDec(a, b) {
   return b.buyPrice - a.buyPrice;
 }
-
-// const jsonResponse = res;
 
 function getPricesOfCoin(coin) {
   const prices = [];
@@ -68,24 +61,80 @@ function getPricesOfCoin(coin) {
 
 function tableCreator(coinPrices, currency, type) {
   let filtered = coinPrices.filter(
-    (price) => price.currency === currency && price[type]
+    (price) => price.currency === currency && price[`${type}Price`]
   );
-  if (type === "sellPrice") {
+  if (type === "sell") {
     filtered.sort(sortByPriceAsc);
-  } else if (type === "buyPrice") {
+  } else if (type === "buy") {
     filtered.sort(sortByPriceDec);
   }
+  const table = document.querySelector(
+    `table#${currency + type.charAt(0).toUpperCase() + type.slice(1)}`
+  );
+  table.innerHTML = `<tr><th>broker</th><th>broker</th><th>coin</th><th>coin</th><th>bestprice</th><th>action</th></tr>`;
 
-  return filtered;
+  filtered.forEach((row) => {
+    const broker = row.broker;
+    const coin = row.coin;
+    const tr = document.createElement("tr");
+    const td_broker_name = document.createElement("td");
+    const td_broker_image = document.createElement("td");
+    const td_coin_name = document.createElement("td");
+    const td_coin_image = document.createElement("td");
+    const td_price = document.createElement("td");
+    const div_broker_name = document.createElement("div");
+    const div_coin_name = document.createElement("div");
+    const sell_btn = document.createElement("button");
+    const buy_btn = document.createElement("button");
+    const td_sell_buy = document.createElement("td");
+    const broker_image = document.createElement("img");
+    broker_image.setAttribute("src", `/brokers/${broker}.jpg`);
+    broker_image.style = "width: 30px;";
+
+    const coin_image = document.createElement("img");
+    coin_image.setAttribute("src", `/coins/${coin}.jpg`);
+    coin_image.style = "width: 30px;";
+
+    buy_btn.innerText = "BUY";
+    sell_btn.innerText = "SELL";
+    div_broker_name.innerText = broker;
+    div_coin_name.innerText = coin;
+
+    td_broker_name.appendChild(div_broker_name);
+    td_coin_name.appendChild(div_coin_name);
+    td_broker_image.appendChild(broker_image);
+    td_coin_image.appendChild(coin_image);
+
+    const buy = row[`${type}Price`];
+
+    td_price.append(buy);
+
+    td_sell_buy.appendChild(buy_btn);
+    td_sell_buy.appendChild(sell_btn);
+
+    tr.appendChild(td_broker_image);
+    tr.appendChild(td_broker_name);
+    tr.appendChild(td_coin_image);
+    tr.appendChild(td_coin_name);
+    tr.appendChild(td_price);
+    tr.appendChild(td_sell_buy);
+    table.appendChild(tr);
+  });
+
+  // return filtered;
 }
 
 inputCoin.addEventListener("change", () => {
-  const coinPrices = getPricesOfCoin("btc");
+  const coin = inputCoin.value;
+  const coinPrices = getPricesOfCoin(coin);
 
-  let sellIRTPrices = tableCreator(coinPrices, "irt", "sellPrice");
-  let sellUSDTPrices = tableCreator(coinPrices, "usdt", "sellPrice");
-  let buyIRTPrices = tableCreator(coinPrices, "irt", "buyPrice");
-  let buyUSDTPrices = tableCreator(coinPrices, "usdt", "buyPrice");
+  let sellIRTPrices = tableCreator(coinPrices, "irt", "sell");
+  let sellUSDTPrices = tableCreator(coinPrices, "usdt", "sell");
+  let buyIRTPrices = tableCreator(coinPrices, "irt", "buy");
+  let buyUSDTPrices = tableCreator(coinPrices, "usdt", "buy");
 
-
+  console.log(sellIRTPrices);
+  console.log(sellUSDTPrices);
+  console.log(buyIRTPrices);
+  console.log(buyUSDTPrices);
 });
